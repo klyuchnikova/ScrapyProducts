80c81926dfaeffa15da7e6a53935774f0af9596e
Ulyana Klyuchnikova <udklyuchnikova@edu.hse.ru>
2022-06-23 00:11:42 +0300

    first scraper ever

--- scrape_products/scrape_products/spiders/vprok_spider.py
+++ scrape_products/scrape_products/spiders/vprok_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrape_products.scrape_products.items import ScrapeProductsItem
 
 
 class VprokSpider(scrapy.Spider):
@@ -6,16 +7,21 @@ class VprokSpider(scrapy.Spider):
     start_urls = ["https://www.vprok.ru/"]
 
     def parse(self, response, **kwargs):
-        PROMO_CLASS = '.xf-product.xf-product--promo.js-product'
-        CARD_CLASS = "xfnew-semiblocks__carousel-card.tns-item.tns-slide-active"
-        PRODUCT_CLASS = '.xf-product'
+        CLASS_PRODUCT = '.xf-product'
+        CLASS_PROMO = '.xf-product--promo.js-product'
+        CLASS_CARD = "xfnew-semiblocks__carousel-card.tns-item.tns-slide-active"
 
         ATTRIBUTE_NAME = 'data-owox-product-name'
-        for brickset in response.css(PRODUCT_CLASS):
+        ATTRIBUTE_LINK = 'data-product-card-url'
+        ATTRIBUTE_CATEGORY_ID = 'data-owox-category-id'
+        ATTRIBUTE_CATEGORY_NAME = 'data-owox-category-name'
+        ATTRIBUTE_AVAILABLE = 'data-owox-is-available'
+
+        item = ScrapeProductsItem()
+        for brickset in response.css(CLASS_PRODUCT):
             NAME_SELECTOR = 'h1 ::text'
             PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
             MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
             IMAGE_SELECTOR = 'img ::attr(src)'
-            yield {
-                'name': brickset.attrib[ATTRIBUTE_NAME]
-            }
+            item['name'] = brickset.attrib[ATTRIBUTE_NAME]
+            yield item
